7fb9abf40fe7f2ef15c1fc57eccb40c7e0c60673
Nisura-00 <it20133504@my.sliit.lk>
2021-08-21 21:44:06 +0530

    MyMusicApp

---
+++ app/src/main/java/com/example/mymusicapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mymusicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button start, stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        start = findViewById(R.id.button);
+        stop = findViewById(R.id.button2);
+
+        start.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this,MyServices.class));
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this,MyServices.class));
+            }
+        });
+
+    }
+}
